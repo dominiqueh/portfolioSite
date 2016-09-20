508f24bdb009c4f76ca6ea43c95b55792c5a569c
dominiqueh <dominiquehorner@gmail.com>
2016-09-20 12:20:36 -0600

    Hooked server.js to index.html properly

--- server.js
+++ server.js
@@ -1,1 +1,57 @@
-console.log("Testing testing testing")
+// =+==+==+==+==+==+==+==+==+==+==+==+==
+// Requires
+//  =+==+==+==+==+==+==+==+==+==+==+==+==
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	logger = require('morgan'),
+	cors	= require ('cors'),
+	path 	= require ('path'),
+	port	=	process.env.PORT ||	1337,
+	mongoose = require('mongoose'),
+	sendgrid = require('sendgrid')('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')
+
+var sampleModel     = require('./models/sampleSchema.js')
+	var SampeSchema = sampleModel.SampeSchema
+	var Sample      = sampleModel.Sample
+
+//  =+==+==+==+==+==+==+==+==+==+==+==+==
+// Application Configuration
+//  =+==+==+==+==+==+==+==+==+==+==+==+==
+mongoose.connect('mongodb://localhost/DominiqueDB', function (err) {
+	console.log('connected to THE MONGODS')
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.static(path.join(__dirname, './web')))
+
+
+//  =+==+==+==+==+==+==+==+==+==+==+==+==
+// Gets and Posts
+//  =+==+==+==+==+==+==+==+==+==+==+==+==
+app.get("/", function(req,res) {
+	res.sendFile('/index.html', {root : "./public/"})
+})
+
+// SENDGRID
+app.post("/sendMessage", function (req, res) {
+	var payload = req.body
+	payload.to = "dominiquehorner@gmail.com"
+	sendgrid.send(payload,function (err,json) {
+		if (err) {
+			return res.send("FAIL")
+		} else {
+			res.send("SUCCESS!!!")
+		}
+	})
+})
+
+// =+==+==+==+==+==+==+==+==+==+==+==+==
+// Creating Server and Listening for Connections
+// =+==+==+==+==+==+==+==+==+==+==+==+==
+app.listen(port, function() {
+        console.log('Server running on port ' + port)
+    })
