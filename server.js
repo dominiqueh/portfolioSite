8d23656286a2b9deb561c3c7c5c47d1c0dd8392e
dominiqueh <dominiquehorner@gmail.com>
2016-12-14 20:16:04 -0700

    test with sendgrid

--- server.js
+++ server.js
@@ -9,7 +9,26 @@ var express = require('express'),
     path = require('path'),
     port = process.env.PORT || 80,
     mongoose = require('mongoose'),
-    sendgrid = require('sendgrid')('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')
+ helper = require('sendgrid').mail,
+  
+from_email = new helper.Email("test@example.com"),
+to_email = new helper.Email("test@example.com"),
+subject = "Sending with SendGrid is Fun",
+content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js"),
+mail = new helper.Mail(from_email, subject, to_email, content),
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON()
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode)
+  console.log(response.body)
+  console.log(response.headers)
+})
 
 var sampleModel = require('./models/sampleSchema.js')
 var SampeSchema = sampleModel.SampeSchema
